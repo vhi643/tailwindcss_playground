d907a6708080a4f58c1ff53ab0a3e747290d5df9
vhi643 <26123780+vhi643@users.noreply.github.com>
2025-08-11 17:14:29 +0000

    tailwind css youtube

---
+++ .devcontainer/postcreate.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install/update Node packages
+# List outdated packages: npm outdated
+# Upgrade package: npm update samplepackage
+    # Upgrade all main packages: npm update
+# Uninstall package: npm uninstall samplepackage
+npm install
